bf16b04076e3d5138ccd57ab9eecd5cda96f0936
Elected0406 <vl.com.md2@mail.ru>
2017-03-28 09:24:41 +0300

    Add project files.

---
+++ Energiejournaal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Energiejournaal.Startup))]
+namespace Energiejournaal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
