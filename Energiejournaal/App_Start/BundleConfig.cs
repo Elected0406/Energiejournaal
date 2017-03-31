20c3a17737da2aa4633b36d964e5771daa145788
Elected0406 <vl.com.md2@mail.ru>
2017-03-31 18:03:02 +0300

    31.03.17

--- Energiejournaal/App_Start/BundleConfig.cs
+++ Energiejournaal/App_Start/BundleConfig.cs
@@ -9,29 +9,28 @@ namespace Energiejournaal
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-{version}.js"));
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/Moment.js",
                       "~/Scripts/daterangepicker.js",
-                      "~/Scripts/bootstrap-datetimepicker.min.js",
-                      "~/Scripts/daterangepicker.js",
-                      "~/Scripts/moment.min.js",
-                      "~/Scripts/SimpleTabs.js",
-                      "~/Scripts/simple-tabs.min.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap-datetimepicker.js",
+                      "~/Scripts/simple-tabs.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/css/bootstrap-datetimepicker.css",
                       "~/Content/css/bootstrap.css",
-                      "~/Content/css/font-awesome.css",                      
+                      "~/Content/css/font-awesome.css",
                       "~/Content/css/site.css"));
         }
     }
