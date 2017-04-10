7bc7f3bdf4bbb0d79407d058fe4e657dbea5301d
Elected0406 <vl.com.md2@mail.ru>
2017-04-10 17:55:26 +0300

    10.04.17(2)

--- Energiejournaal/Scripts/app.js
+++ Energiejournaal/Scripts/app.js
@@ -1,50 +1,26 @@
 /// <reference path="typings/jquery/jquery.d.ts" />
 var temperatures = [7, 1.7, 1.3, 1.3, 1.2, 0.9, 0.6, 0.6, 0.6, 1.2, 3.3, 6.6, 8.7, 10.5];
 var name = 'Power Belgium Spot EUR/MWh';
-//$('#Groups').change(function () {
-//    // получаем выбранный id
-//    var id = $(this).val();
-//    $.ajax({
-//        type: 'GET',
-//        url: 'http://localhost:4051/Home/GetCharts' +'?id=' + id,
-//        success: function (data) {
-//            // заменяем содержимое присланным частичным представлением
-//            $('#Chart').replaceWith(data);
-//            $('#Chart').change(function () {
-//                var id = $(this).val();
-//                $.ajax({
-//                    type: 'GET',
-//                    url: 'http://localhost:4051/Home/GetData' + '?id=' + id,
-//                    success: function (data) {
-//                        $('#Data').replaceWith(data);
-//                    }
-//                });
-//            });
-//        }
-//    });
-//});
-var GroupsList = (function () {
-    function GroupsList() {
-    }
-    return GroupsList;
-}());
 var ChartsList = (function () {
     function ChartsList() {
         this.charts = new Array();
+        this.chartdata = new Array();
     }
     ChartsList.prototype.UpdateChart = function () {
-        var _this = this;
-        var id = $(this).val();
-        $.getJSON('http://localhost:4051/Home/GetCharts' + '?id=' + id, function (data) {
-            _this.charts = data;
-            var select = '<select class="form-control">';
-            for (var i = 0; i < _this.charts.length; i++) {
-                var selectRow = '<option>' + _this.charts[i].Name + '</option>';
-            }
-            select += '<option disabled selected> Select Chart</option>';
-            select += selectRow;
-            select += '</select>';
-            $("#Chart").replaceWith(select);
+        $("#Groups").change(function () {
+            var _this = this;
+            var id = $(this).val();
+            $.getJSON('http://localhost:4051/Home/GetCharts' + '?id=' + id, function (data) {
+                _this.charts = data;
+                var select = '<select class="form-control">';
+                for (var i = 0; i < _this.charts.length; i++) {
+                    var selectRow = '<option>' + _this.charts[i].Name + '</option>';
+                }
+                select += '<option disabled selected> Select Chart</option>';
+                select += selectRow;
+                select += '</select>';
+                $("#Chart").replaceWith(select);
+            });
         });
     };
     return ChartsList;
@@ -54,37 +30,12 @@ var vwChart = (function () {
     }
     return vwChart;
 }());
-var UserList = (function () {
-    function UserList() {
-        this.users = new Array();
+var vwData = (function () {
+    function vwData() {
     }
-    UserList.prototype.displayUsers = function () {
-        var _this = this;
-        $.getJSON('http://localhost:4051/Home/GetUsers', function (data) {
-            _this.users = data;
-            var table = '<table class="table">';
-            for (var i = 0; i < _this.users.length; i++) {
-                var tableRow = '<tr>' +
-                    '<td>' + _this.users[i].Id + '</td>' +
-                    '<td>' + _this.users[i].Name + '</td>' +
-                    '<td>' + _this.users[i].Age + '</td>' +
-                    '</tr>';
-                table += tableRow;
-            }
-            table += '</table>';
-            $('#Data').html(table);
-        });
-    };
-    return UserList;
-}());
-var User = (function () {
-    function User() {
-    }
-    return User;
+    return vwData;
 }());
 window.onload = function () {
-    var userList = new UserList();
     var chartsList = new ChartsList();
-    $("#displayBtn").click(function () { userList.displayUsers(); });
-    $("#displayBtn2").click(function () { chartsList.UpdateChart(); });
+    chartsList.UpdateChart();
 };
--- Energiejournaal/Scripts/app.ts
+++ Energiejournaal/Scripts/app.ts
@@ -1,48 +1,26 @@
 ﻿/// <reference path="typings/jquery/jquery.d.ts" />
 var temperatures = [7, 1.7, 1.3, 1.3, 1.2, 0.9, 0.6, 0.6, 0.6, 1.2, 3.3, 6.6, 8.7, 10.5];
 var name = 'Power Belgium Spot EUR/MWh';
-//$('#Groups').change(function () {
-//    // получаем выбранный id
-//    var id = $(this).val();
-//    $.ajax({
-//        type: 'GET',
-//        url: 'http://localhost:4051/Home/GetCharts' +'?id=' + id,
-//        success: function (data) {
-//            // заменяем содержимое присланным частичным представлением
-//            $('#Chart').replaceWith(data);
-//            $('#Chart').change(function () {
-//                var id = $(this).val();
-//                $.ajax({
-//                    type: 'GET',
-//                    url: 'http://localhost:4051/Home/GetData' + '?id=' + id,
-//                    success: function (data) {
-//                        $('#Data').replaceWith(data);
-//                    }
-//                });
-//            });
-//        }
-//    });
-//});
-class GroupsList {
-
-}
 class ChartsList {
     private charts: Array<vwChart> = new Array<vwChart>();
+    private chartdata: Array<vwData> = new Array<vwData>();
     UpdateChart(): void {
-        var id = $(this).val();
-        $.getJSON('http://localhost:4051/Home/GetCharts' + '?id=' + id,
-            (data) => {
-                this.charts = data;
-                var select = '<select class="form-control">'
-                for (var i = 0; i < this.charts.length; i++) {
-                    var selectRow = 
-                    '<option>' + this.charts[i].Name + '</option>';                    
-                }
-                select += '<option disabled selected> Select Chart</option>';
-                select += selectRow;
-                select += '</select>';
-                $("#Chart").replaceWith(select);
-            });        
+        $("#Groups").change(function () {
+            var id = $(this).val();
+            $.getJSON('http://localhost:4051/Home/GetCharts' + '?id=' + id,
+                (data) => {
+                    this.charts = data;
+                    var select = '<select class="form-control">'
+                    for (var i = 0; i < this.charts.length; i++) {
+                        var selectRow =
+                            '<option>' + this.charts[i].Name + '</option>';
+                    }
+                    select += '<option disabled selected> Select Chart</option>';
+                    select += selectRow;
+                    select += '</select>';
+                    $("#Chart").replaceWith(select);
+                });
+        });
     }
 }
 class vwChart {
@@ -53,37 +31,12 @@ class vwChart {
     Period: number;
     PeriodName: string;
 }
-class UserList {
-
-    private users: Array<User> = new Array<User>();
-    displayUsers(): void {
-        $.getJSON('http://localhost:4051/Home/GetUsers',
-            (data) => {
-                this.users = data;
-                var table = '<table class="table">'
-                for (var i = 0; i < this.users.length; i++) {
-
-                    var tableRow = '<tr>' +
-                        '<td>' + this.users[i].Id + '</td>' +
-                        '<td>' + this.users[i].Name + '</td>' +
-                        '<td>' + this.users[i].Age + '</td>' +
-                        '</tr>';
-                    table += tableRow;
-                }
-                table += '</table>';
-                $('#Data').html(table);
-            });
-    }
-}
-class User {
-
-    Id: number;
-    Name: string;
-    Age: number;
+class vwData {
+    Chart: number;
+    ChartName: string;
+    Value: number;
 }
 window.onload = () => {
-    var userList: UserList = new UserList();
     var chartsList: ChartsList = new ChartsList();
-    $("#displayBtn").click(() => { userList.displayUsers(); });
-    $("#displayBtn2").click(() => { chartsList.UpdateChart(); });
+    chartsList.UpdateChart();
 }
